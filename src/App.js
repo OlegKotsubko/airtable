31bdcd11d33c3118845cd43bc725e4fbff2a3b80
Oleg Kotsubko <olegkotsubko@gmail.com>
2022-03-31 02:05:30 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {Link} from "react-router-dom";
+
+import useRecordContext from "./hooks/useRecordContext";
+
 import './App.css';
 
 function App() {
+  const {state} = useRecordContext()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {state.map(record => {
+        return (
+          <div key={record.id} className="item">
+            <Link to={`record/${record.id}`} className="link">
+              <div className="page">
+                <div className="page__logo">
+                  {record.fields.Logo.map(logo => (
+                    <img key={logo.id} src={logo.url} alt=""/>
+                  ))}
+                </div>
+                <div className="page__title">{record.fields.Action}</div>
+              </div>
+
+            </Link>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as ReactDOMClient from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.render(
+import './index.css';
+import {RecordContextProvider} from "./contexts/RecordContext";
+
+import App from './App';
+import Page from "./Page";
+
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RecordContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="record/:id" element={<Page />} />
+        </Routes>
+      </BrowserRouter>
+    </RecordContextProvider>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
